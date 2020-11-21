ad0d4714a9a68ce7c833cd5d031884949b419504
anemoneflower <sallylucky@naver.com>
2020-11-21 17:20:27 +0900

    ADD: get currentuserinfo after login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
 <script>
 // @ is an alias to /src
 import SearchBar from "@/components/SearchBar_Main.vue";
+import firebase from "firebase";
 export default {
   name: "Home",
   components: {
@@ -19,6 +20,19 @@ export default {
     bookKey: {
       type: String,
       default: "none"
+    }
+  },
+  created() {
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+      });
     }
   },
   methods: {
